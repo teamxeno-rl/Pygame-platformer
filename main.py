e680d93f2cee8318cdbf3b425159684fd17e1483
YABUTO <56931098+teamxeno-rl@users.noreply.github.com>
2023-07-02 19:36:39 -0400

    fdjf

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ while True:
             super().__init__()
             self.surf = pygame.Surface((30,30))
             self.surf.fill((128,255,40))
-            self.rect = self.surf.get_rect(center = (20, 420))
+            self.rect = self.surf.get_rect(center = (30, 420))
 
             self.pos = vec((20, 670))
             self.vel = vec(0,0)
