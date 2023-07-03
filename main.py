1023e55b24fd38180f4842175135723e57881f9f
YABUTO <56931098+teamxeno-rl@users.noreply.github.com>
2023-07-02 22:12:46 -0400

    final (4 now)

--- main.py
+++ main.py
@@ -11,8 +11,9 @@ green = (0, 255, 0)
 blue = (0, 0, 128)
 
 best = 0
+bestscore = 0
 while True:
-
+    best = bestscore
     vec = pygame.math.Vector2
     font = pygame.font.Font('ARIAL.TTF', 32)
     HEIGHT = 800
@@ -35,7 +36,11 @@ while True:
             super().__init__()
             self.surf = pygame.Surface((30,30))
             self.surf.fill((128,255,40))
-            self.rect = self.surf.get_rect(center = (30, 420))
+
+            img_path = f'player.png'
+            self.img = pygame.image.load(img_path)
+            self.img = pygame.transform.scale(self.img, (30, 30))
+            self.rect = self.img.get_rect(center = (30, 420))
 
             self.pos = vec((20, 670))
             self.vel = vec(0,0)
@@ -64,21 +69,21 @@ while True:
             self.rect.midbottom = self.pos
         
         def update(self):
-            hits = pygame.sprite.spritecollide(P1, platforms, False)
-            if P1.vel.y > 0:
+            hits = pygame.sprite.spritecollide(PL1, platforms, False)
+            if PL1.vel.y > 0:
                 if hits:
                     self.vel.y = 0
                     self.pos.y = hits[0].rect.top + 1
         
         def jump(self):
-            hits = pygame.sprite.spritecollide(P1, platforms, False)
+            hits = pygame.sprite.spritecollide(PL1, platforms, False)
             if hits:
                 self.vel.y = -18
 
     class platform(pygame.sprite.Sprite):
         def __init__(self):
             super().__init__()
-            self.surf = pygame.Surface((random.randint(100,250), 24))
+            self.surf = pygame.Surface((random.randint(100,250), 10))
             self.surf.fill((128,0,40))
             self.rect = self.surf.get_rect(center = (random.randint(0,WIDTH-20), random.randint(0,HEIGHT-60)))
 
@@ -91,18 +96,18 @@ while True:
                 all_sprites.add(p)
 
     PT1 = platform ()
-    P1 = Player()
+    PL1 = Player()
 
     #make floor
     PT1.surf = pygame.Surface((WIDTH, 20))
     PT1.surf.fill((128,0,40))
-    PT1.rect = PT1.surf.get_rect(center = (WIDTH/2, HEIGHT - 20))
-
+    PT1.rect = PT1.surf.get_rect(center = (WIDTH/2, HEIGHT - 10))
+    players = pygame.sprite.Group()
     platforms = pygame.sprite.Group()
     all_sprites = pygame.sprite.Group()
     platforms.add(PT1)
     all_sprites.add(PT1)
-    all_sprites.add(P1)
+    players.add(PL1)
 
     for x in range(random.randint(6,7)):
         pl = platform()
@@ -117,12 +122,12 @@ while True:
                     pygame.quit()
                     sys.exit()
                 if event.key == pygame.K_UP:
-                    P1.jump()
+                    PL1.jump()
         
-        if P1.rect.top <= HEIGHT / 3:
-            P1.pos.y += abs(P1.vel.y)
+        if PL1.rect.top <= HEIGHT / 3:
+            PL1.pos.y += abs(PL1.vel.y)
             for plat in platforms:
-                plat.rect.y += abs(P1.vel.y)
+                plat.rect.y += abs(PL1.vel.y)
                 if plat.rect.top >= HEIGHT:
                     platform.plat_gen()
                     Score = int(Score) + 1
@@ -130,29 +135,40 @@ while True:
                     text = font.render("SCORE: " + str(Scre), True, white)
                     if Score > bestscore:
                         bestscore = Score
-                        best = bestscore
                         besti = font.render("BEST SCORE: " + str(bestscore), True, white)
                     plat.kill()
         
-        if P1.rect.top > HEIGHT:
+        if PL1.rect.top > HEIGHT:
             for entity in all_sprites:
                 entity.kill()
                 time.sleep(1)
                 displaysurface.fill((255,0,0))
-                text = font.render("FINAL SCORE: " + str(Scre), True, white)
-                displaysurface.blit(text, (120,350))
+                if Score < best:
+                    text = font.render("FINAL SCORE: " + str(Scre), True, white)
+                    text2 = font.render("BEST SCORE: " + str(bestscore), True, green)
+                    displaysurface.blit(text, (120,300))
+                    displaysurface.blit(text2, (120,350))
+                elif Score == best:
+                    text = font.render("FINAL SCORE: " + str(Scre), True, green)
+                    displaysurface.blit(text, (120,300))
+                elif Score > best:
+                    text = font.render("NEW HIGH SCORE: " + str(Scre), True, green)
+                    displaysurface.blit(text, (120,300))
                 pygame.display.update()
                 time.sleep(5)
                 run1 = bool(0)
                 
         displaysurface.fill((0,0,0))
 
-        for entity in all_sprites:
+        for entity in platforms:
             displaysurface.blit(entity.surf, entity.rect)
+        
+        for entity in players:
+            displaysurface.blit(entity.img, entity.rect)
 
         displaysurface.blit(text, (200,50))
         displaysurface.blit(besti, (200,100))
-        P1.move()
-        P1.update()
+        PL1.move()
+        PL1.update()
         pygame.display.update()
         FramePerSec.tick(FPS)
